d5dd6ae30b169e2c9ca9b8f62af858cc9fcf61e4
yuyu-gif <1547140182@qq.com>
2020-11-13 16:10:12 +0800

    update-blog

---
+++ src/components/ShowBlogs.vue
@@ -0,0 +1,82 @@
+<template>
+  <div v-theme:column="'narrow'" id="show-blogs">
+    <h1>博客总览</h1>
+    <input type="text" v-model="search" placeholder="搜索" />
+    <div v-for="blog in filterBlogs" class="single-blog">
+      <router-link v-bind:to="'/blog/' + blog.id">
+        <h2 v-rainbow>{{ blog.title | touppercase }}</h2>
+      </router-link>
+      <article>
+        {{ blog.body | snippet }}
+      </article>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "show-blogs",
+  data() {
+    return {
+      blogs: [],
+      search: "",
+    };
+  },
+  created() {
+    this.$http
+      .get("./../static/posts.json")
+      // .get("http://jsonplaceholder.typicode.com/posts")
+      .then(function (data) {
+        //console.log(data);
+        this.blogs = data.body.slice(0, 10);
+        //console.log(this.blogs);
+      });
+  },
+  computed: {
+    filterBlogs: function () {
+      return this.blogs.filter((blog) => {
+        return blog.title.match(this.search);
+      });
+    },
+  },
+  // filters: {
+  //   "touppercase":function(value){
+  //     return value.toUpperCase();
+  //   }
+  //   toUppercase(value) {
+  //     return value.toUpperCase();
+  //   },
+  // },
+  // directives: {
+  //   rainbow: {
+  //     bind(el, binding, vnode) {
+  //       el.style.color = "#" + Math.random().toString(16).slice(2, 8);
+  //     },
+  //   },
+  // },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#show-blogs {
+  max-width: 800px;
+  margin: 0 auto;
+}
+.single-blog {
+  padding: 20px;
+  margin: 20px 0;
+  box-sizing: border-box;
+  background: #eee;
+  border: 1px dotted #aaa;
+}
+#show-blogs a {
+  color: #444;
+  text-decoration: none;
+}
+input[type="text"] {
+  padding: 8px;
+  width: 100%;
+  box-sizing: border-box;
+}
+</style>
---
+++ src/routes.js
@@ -0,0 +1,9 @@
+import AddBlog from './components/AddBlog.vue'
+import ShowBlogs from './components/ShowBlogs.vue'
+import SingleBlog from './components/SingleBlog.vue'
+
+export default [
+    { path: "/", component: ShowBlogs },
+    { path: "/add", component: AddBlog },
+    {path:"/blog/:id",component:SingleBlog},
+]
